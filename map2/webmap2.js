447a5473692b4f3db6e3e18052f2d41723674393
Luke E Driskell <luke@lsu.edu>
2019-03-28 11:58:30 -0500

    Update

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,8 +1,11 @@
 let myMap = L.map('map2').setView([32.18, -99.14], 4)
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
+// L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+//   layers: 'nexrad-n0r-900913',
+//   format: 'image/png',
+//   transparent: true,
+//   attribution: 'NOAA, Iowa State University'
+// }).addTo(myMap)
+L.esri.dynamicMapLayer({
+  url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
 }).addTo(myMap)
